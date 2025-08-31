e9b6d28bc36dd8ebc2ae0cac2b99f287bc476889
JoaquinMontero96 <joaquingmontero1996@gmail.com>
2025-08-31 12:02:14 -0300

    Ejercicio 4 TP 3

--- 03 ESTRUCTURAS CONDICIONALES/MONTERO_TP-03.py
+++ 03 ESTRUCTURAS CONDICIONALES/MONTERO_TP-03.py
@@ -12,7 +12,7 @@ if nota_usuario >= 6:
     print("Aprobado")
 else:
     print("Desaprobado")
-"""
+
 #Ejercicio 3
 numero_usuario = int(input("Ingrese un numero par: "))
 
@@ -20,3 +20,15 @@ if numero_usuario % 2 == 0:
     print("Ha ingresado un número par")
 else:
     print("Por favor ingrese un número par")
+"""
+#Ejercicio 4
+edad_usuario = int(input("Ingrese su edad: "))
+
+if edad_usuario < 12:
+    print("Usted es niño/a")
+elif edad_usuario >= 12 and edad_usuario < 18:
+    print("Usted es adolescente")
+elif edad_usuario >= 18 and edad_usuario < 30:
+    print("Usted es adulto/a joven")
+else:
+    print("Usted es adulto/a")
