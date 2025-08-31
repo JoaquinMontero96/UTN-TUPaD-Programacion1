068f0cfb8a37d7c30616950e35b684af94b92fda
JoaquinMontero96 <joaquingmontero1996@gmail.com>
2025-08-31 11:54:27 -0300

    Ejercicio 2 TP 3

--- 03 ESTRUCTURAS CONDICIONALES/MONTERO_TP-03.py
+++ 03 ESTRUCTURAS CONDICIONALES/MONTERO_TP-03.py
@@ -1,5 +1,15 @@
 #Ejercicio 1
+"""
 edad_usuario = int(input("Ingrese su edad: "))
 
 if edad_usuario >= 18:
     print("Es mayor de edad.")
+"""
+
+#Ejercicio 2
+nota_usuario = int(input("Ingrese su nota: "))
+
+if nota_usuario >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
