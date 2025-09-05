c5283be1e4c31a9756df020db9e04fdb32e268d7
JoaquinMontero96 <joaquingmontero1996@gmail.com>
2025-09-04 23:50:12 -0300

    Ejercicio 9 TP 4

--- 04 ESTRUCTURAS REPETITIVAS/MONTERO_TP-04.py
+++ 04 ESTRUCTURAS REPETITIVAS/MONTERO_TP-04.py
@@ -65,23 +65,34 @@
 # print(f"La sumatoria de todos los números enteros comprendidos entre 0 y {numero_usuario} es {suma}")
 
 # Ejercicio 8
-pares = 0
-impares = 0
-positivos = 0
-negativos = 0
+# pares = 0
+# impares = 0
+# positivos = 0
+# negativos = 0
+# cant_num = 100
+
+# for cont in range(1, cant_num+1):
+#     num_usuario = int(input(f"Ingrese número {cont}: "))
+
+#     if num_usuario % 2 == 0:
+#         pares += 1
+#     else:
+#         impares += 1
+
+#     if num_usuario > 0:
+#         positivos += 1
+#     elif num_usuario < 0:
+#         negativos += 1
+
+# print(f"En los {cant_num} números que ingresaste hay {pares} pares y {impares} impares. A su vez {positivos} positivos y {negativos} negativos.")
+
+# Ejercicio 9
+import statistics
 cant_num = 100
+numeros = []
 
 for cont in range(1, cant_num+1):
     num_usuario = int(input(f"Ingrese número {cont}: "))
+    numeros.append(num_usuario)
 
-    if num_usuario % 2 == 0:
-        pares += 1
-    else:
-        impares += 1
-
-    if num_usuario > 0:
-        positivos += 1
-    elif num_usuario < 0:
-        negativos += 1
-
-print(f"En los {cant_num} números que ingresaste hay {pares} pares y {impares} impares. A su vez {positivos} positivos y {negativos} negativos.")
+print(f"La media de los números ingresados es {statistics.mean(numeros)}")
