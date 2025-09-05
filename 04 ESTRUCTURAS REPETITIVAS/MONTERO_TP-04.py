0282728d19e56e48632dae4dfaaec95846d1f6ab
JoaquinMontero96 <joaquingmontero1996@gmail.com>
2025-09-04 23:04:46 -0300

    Ejercicio 2 TP 4

--- 04 ESTRUCTURAS REPETITIVAS/MONTERO_TP-04.py
+++ 04 ESTRUCTURAS REPETITIVAS/MONTERO_TP-04.py
@@ -1,4 +1,13 @@
 # Ejercicio 1
-for cont in range(101):
-    if cont % 2 == 0:
-        print(cont)
+# for cont in range(101):
+#     if cont % 2 == 0:
+#         print(cont)
+
+# Ejercicio 2
+cont = 0
+numero_usuario = input("Ingrese un número entero: ")
+
+for digito in numero_usuario:
+    cont += 1
+
+print(f"{numero_usuario} tiene {cont} dígitos")
