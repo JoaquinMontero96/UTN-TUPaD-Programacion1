5bd0855dbe57305b631ee5c7f6c011ce18540ac6
JoaquinMontero96 <joaquingmontero1996@gmail.com>
2025-09-04 23:08:33 -0300

    Ejercicio 3 TP 4

--- 04 ESTRUCTURAS REPETITIVAS/MONTERO_TP-04.py
+++ 04 ESTRUCTURAS REPETITIVAS/MONTERO_TP-04.py
@@ -4,10 +4,20 @@
 #         print(cont)
 
 # Ejercicio 2
-cont = 0
-numero_usuario = input("Ingrese un número entero: ")
+# cont = 0
+# numero_usuario = input("Ingrese un número entero: ")
 
-for digito in numero_usuario:
-    cont += 1
+# for digito in numero_usuario:
+#     cont += 1
 
-print(f"{numero_usuario} tiene {cont} dígitos")
+# print(f"{numero_usuario} tiene {cont} dígitos")
+
+# Ejercicio 3
+suma = 0
+num1 = int(input("Ingrese un número entero: "))
+num2 = int(input("Ingrese otro número entero: "))
+
+for cont in range(num1+1, num2):
+    suma += cont
+
+print(f"La suma de todos los números enteros comprendidos entre {num1} y {num2} (sin incluirlos) es {suma}")
