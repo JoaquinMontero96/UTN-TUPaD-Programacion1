0705ede78bf09636de348e89562f12bba6a558bc
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-09-18 20:38:36 -0300

    Ejercicio 3 TP 5

--- 05 LISTAS/MONTERO_TP-05.py
+++ 05 LISTAS/MONTERO_TP-05.py
@@ -25,25 +25,46 @@
 # #Impresión por panatalla de nota mínima y máxima
 # print(f"La nota mas baja es {nota_minima} y la nota mas alta es {nota_maxima}")
 
-# Ejercicio 2
+# # Ejercicio 2
 
-lista_productos = []
+# #Definimos las variables
+# lista_productos = []
 
-#Solicitud de 5 productos
-for i in range(5):
-    producto = input(f"Ingrese el producto {i+1}: ")
-    #Adición de cada producto a la lista
-    lista_productos.append(producto)
+# #Iteramos 5 veces para solicitar productos al usuario y agregarlos a una lista
+# for i in range(5):
+#     producto = input(f"Ingrese el producto {i+1}: ")
+#     lista_productos.append(producto)
 
-#Orden de lista y muestra por pantalla
-lista_productos = sorted(lista_productos)
-print(lista_productos)
+# #Ordenamos la lista de menor a mayor y la mostramos por pantalla
+# lista_productos = sorted(lista_productos)
+# print(lista_productos)
 
-elemento_aeliminar = input("¿Qué producto desea eliminar? ")
+# #Consultamos qué elemento quiere eliminar el usuario
+# elemento_aeliminar = input("¿Qué producto desea eliminar? ")
 
-if elemento_aeliminar in lista_productos:
-    lista_productos.remove(elemento_aeliminar)
-    print(f"{elemento_aeliminar} fue eliminado con éxito")
-    print(lista_productos)
-else:
-    print("Producto no encontrado")
+# #Comprobamos si el elemento está en la lista, lo eliminamos e informamos
+# if elemento_aeliminar in lista_productos:
+#     lista_productos.remove(elemento_aeliminar)
+#     print(f"{elemento_aeliminar} fue eliminado con éxito")
+#     print(lista_productos)
+# else:
+#     print("Producto no encontrado")
+
+# Ejercicio 3
+import random
+
+#Definimos las variables
+cant_numeros = 15
+lista_pares = []
+lista_impares = []
+
+#Iteramos x veces y generamos x números aleatorios, luego comprobamos si son pares o impares y lo agregamos a la lista correspondiente
+for i in range(cant_numeros):
+    numero_aleatorio = random.randint(1, 100)
+    if numero_aleatorio % 2 == 0:
+        lista_pares.append(numero_aleatorio)
+    else:
+        lista_impares.append(numero_aleatorio)
+
+#Mostramos la cantidad de elementos de cada lista
+print(f"Hay {len(lista_pares)} números pares y {len(lista_impares)} números impares")
