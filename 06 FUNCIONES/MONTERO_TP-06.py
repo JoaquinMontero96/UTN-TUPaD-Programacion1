e4bff900722f652d10dccd7a5db52e768fd13631
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-10-05 12:42:29 -0300

    Ejercicio 3 TP 6

--- 06 FUNCIONES/MONTERO_TP-06.py
+++ 06 FUNCIONES/MONTERO_TP-06.py
@@ -1,12 +1,24 @@
-# Ejercicio 1
+# # Ejercicio 1
 # def imprimir_hola_mundo():
 #     print("Hola Mundo!")
 
 # imprimir_hola_mundo()
 
-#Ejercicio 2
-def saludar_usuario(nombre):
-    print(f"Hola {nombre}!")
+# #Ejercicio 2
+# def saludar_usuario(nombre):
+#     print(f"Hola {nombre}!")
 
-nombre = input("Cual es tu nombre? ")
-saludar_usuario(nombre)
+# nombre = input("Cual es tu nombre? ")
+# saludar_usuario(nombre)
+
+# Ejercicio 3
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+print("Ingrese sus datos")
+nombre = input("Nombre: ")
+apellido = input("Apellido: ")
+edad = input("Edad: ")
+residencia = input("Lugar de residencia: ")
+
+informacion_personal(nombre, apellido, edad, residencia)
