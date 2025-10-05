ff34081e11dff5544f5feda4c6bff5b29204d5c6
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-10-05 12:47:32 -0300

    Ejercicio 4 TP 6

--- 06 FUNCIONES/MONTERO_TP-06.py
+++ 06 FUNCIONES/MONTERO_TP-06.py
@@ -11,14 +11,28 @@
 # nombre = input("Cual es tu nombre? ")
 # saludar_usuario(nombre)
 
-# Ejercicio 3
-def informacion_personal(nombre, apellido, edad, residencia):
-    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+# # Ejercicio 3
+# def informacion_personal(nombre, apellido, edad, residencia):
+#     print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
-print("Ingrese sus datos")
-nombre = input("Nombre: ")
-apellido = input("Apellido: ")
-edad = input("Edad: ")
-residencia = input("Lugar de residencia: ")
+# print("Ingrese sus datos")
+# nombre = input("Nombre: ")
+# apellido = input("Apellido: ")
+# edad = input("Edad: ")
+# residencia = input("Lugar de residencia: ")
 
-informacion_personal(nombre, apellido, edad, residencia)
+# informacion_personal(nombre, apellido, edad, residencia)
+
+# Ejercicio 4
+import math
+def calcular_area_circulo(radio):
+    return math.pi * radio**2
+
+def calcular_perimetro_circulo(radio):
+    return math.pi * 2 * radio
+
+radio = float(input("Ingrese el radio del círculo: "))
+area = round(calcular_area_circulo(radio), 2)
+perimetro = round(calcular_perimetro_circulo(radio), 2)
+
+print(f"El área del círculo es {area} y el perímetro {perimetro}")
