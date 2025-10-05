b59e23a9db433c53e2a5e989b20e1ff9d0b4e4cd
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-10-05 12:39:05 -0300

    Ejercicio 2 TP 6

--- 06 FUNCIONES/MONTERO_TP-06.py
+++ 06 FUNCIONES/MONTERO_TP-06.py
@@ -1,5 +1,12 @@
 # Ejercicio 1
-def imprimir_hola_mundo():
-    print("Hola Mundo!")
+# def imprimir_hola_mundo():
+#     print("Hola Mundo!")
 
-imprimir_hola_mundo()
+# imprimir_hola_mundo()
+
+#Ejercicio 2
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}!")
+
+nombre = input("Cual es tu nombre? ")
+saludar_usuario(nombre)
