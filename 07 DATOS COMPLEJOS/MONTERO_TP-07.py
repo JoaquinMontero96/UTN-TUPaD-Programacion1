d7431606444c079f0432d7ba27083602e061c9b1
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-10-15 21:23:37 -0300

    Ejercicio 5 TP 7

--- 07 DATOS COMPLEJOS/MONTERO_TP-07.py
+++ 07 DATOS COMPLEJOS/MONTERO_TP-07.py
@@ -1,34 +1,45 @@
-# Ejercicio 1
-precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
-precios_frutas["Naranja"] = 1200
-precios_frutas["Manzana"] = 1500
-precios_frutas["Pera"] = 2300
+# # Ejercicio 1
+# precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+# precios_frutas["Naranja"] = 1200
+# precios_frutas["Manzana"] = 1500
+# precios_frutas["Pera"] = 2300
 
-print(precios_frutas)
+# print(precios_frutas)
 
-# Ejercicio 2
-precios_frutas["Banana"] = 1330
-precios_frutas["Manzana"] = 1700
-precios_frutas["Melón"] = 2800
+# # Ejercicio 2
+# precios_frutas["Banana"] = 1330
+# precios_frutas["Manzana"] = 1700
+# precios_frutas["Melón"] = 2800
 
-print(precios_frutas)
+# print(precios_frutas)
 
-# Ejercicio 3
-lista_frutas = list(precios_frutas.keys())
+# # Ejercicio 3
+# lista_frutas = list(precios_frutas.keys())
 
-print(lista_frutas)
+# print(lista_frutas)
 
-# Ejercicio 4
-lista_contactos = {}
+# # Ejercicio 4
+# lista_contactos = {}
 
-for i in range(5):
-    nombre = input("Ingrese nombre del contacto: ")
-    num = int(input("Ingrese número de contacto: "))
+# for i in range(5):
+#     nombre = input("Ingrese nombre del contacto: ")
+#     num = int(input("Ingrese número de contacto: "))
 
-    lista_contactos[nombre] = num
+#     lista_contactos[nombre] = num
 
-print(lista_contactos)
+# print(lista_contactos)
 
-nombre_consulta = input("Ingrese nombre a consultar: ")
-if nombre_consulta in lista_contactos:
-    print(lista_contactos[nombre_consulta])
+# nombre_consulta = input("Ingrese nombre a consultar: ")
+# if nombre_consulta in lista_contactos:
+#     print(lista_contactos[nombre_consulta])
+
+#Ejercicio 5
+from collections import Counter
+
+frase = input("Ingrese una frase: ")
+listado_palabras = frase.split()
+palabras = set(listado_palabras)
+cantidad_palabras = dict(Counter(listado_palabras))
+
+print(palabras)
+print(cantidad_palabras)
