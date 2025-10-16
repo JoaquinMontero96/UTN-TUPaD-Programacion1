e8322d6ab663f5a7661ceb870452a736d38e6eab
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-10-16 20:58:50 -0300

    Ejercicio 8 TP 7

--- 07 DATOS COMPLEJOS/MONTERO_TP-07.py
+++ 07 DATOS COMPLEJOS/MONTERO_TP-07.py
@@ -33,7 +33,7 @@
 # if nombre_consulta in lista_contactos:
 #     print(lista_contactos[nombre_consulta])
 
-# #Ejercicio 5
+# # Ejercicio 5
 # from collections import Counter
 
 # frase = input("Ingrese una frase: ")
@@ -44,7 +44,7 @@
 # print(palabras)
 # print(cantidad_palabras)
 
-# Ejercicio 6
+# # Ejercicio 6
 
 # alumnos = {}
 
@@ -62,14 +62,72 @@
 #     promedio = round(sum(notas)/len(notas), 2)
 #     print(f"El promedio de {alumno} es {promedio}")
 
-# Ejercicio 7
+# # Ejercicio 7
 
-primer_parcial = {1, 2, 4, 9, 14, 15, 16, 18, 20}
-segundo_parcial = {1, 3, 4, 5, 6, 8, 11, 14, 15, 16, 17, 18, 19}
+# primer_parcial = {1, 2, 4, 9, 14, 15, 16, 18, 20}
+# segundo_parcial = {1, 3, 4, 5, 6, 8, 11, 14, 15, 16, 17, 18, 19}
 
-interseccion = primer_parcial & segundo_parcial
-diferencia_simetrica = primer_parcial ^ segundo_parcial
-union = primer_parcial | segundo_parcial
-print(f"Lista de alumnos que aprobaron ambos parciales: {interseccion}")
-print(f"Lista de alumnos que aprobaron solo un parcial: {diferencia_simetrica}")
-print(f"Lista de alumnos que aprobaron al menos un parcial: {union}")
+# interseccion = primer_parcial & segundo_parcial
+# diferencia_simetrica = primer_parcial ^ segundo_parcial
+# union = primer_parcial | segundo_parcial
+# print(f"Lista de alumnos que aprobaron ambos parciales: {interseccion}")
+# print(f"Lista de alumnos que aprobaron solo un parcial: {diferencia_simetrica}")
+# print(f"Lista de alumnos que aprobaron al menos un parcial: {union}")
+
+# Ejercicio 8
+import time
+
+def consultar_stock(articulo):
+    if articulo in stock:
+        print(f"El stock de {articulo} es {stock[articulo]}")
+    else:
+        print(f"{articulo} no está cargado en el inventario")
+
+def actualizar_stock(articulo):
+    if articulo in stock:
+        cantidad_actualizar = int(input("Ingrese cantidad a ingresar (en negativo para restar): "))
+        stock[articulo] += cantidad_actualizar
+        print("Stock actualizado con éxito")
+    else:
+        print(f"{articulo} no está cargado en el inventario")
+
+def agregar_articulo(articulo):
+    if articulo in stock:
+        print(f"{articulo} ya se encuentra cargado en el inventario")
+    else:
+        stock_inicial = int(input("Ingrese el stock inicial: "))
+        stock[articulo] = stock_inicial
+        print(f"{articulo} fue ingresado con éxito")
+
+stock = {
+    "Tenedor": 120,
+    "Cuchillo": 100,
+    "Cuchara": 90,
+    "Vaso": 200,
+    "Taza": 120
+}
+
+while True:
+    print("-----------------------")
+    print("Ingrese una opción:")
+    print("CONSULTAR STOCK - 1")
+    print("ACTUALIZAR STOCK - 2")
+    print("AGREGAR ARTÍCULO - 3")
+    print("SALIR - 0")
+    print("-----------------------")
+    opcion_usuario = int(input())
+    match opcion_usuario:
+        case 0:
+            break
+        case 1:
+            articulo = input("Ingrese el nombre del artículo a consultar: ")
+            consultar_stock(articulo)
+        case 2:
+            articulo = input("Ingrese el nombre del artículo a actualizar: ")
+            actualizar_stock(articulo)
+        case 3:
+            articulo = input("Ingrese el nombre del artículo que desea agregar al inventario: ")
+            agregar_articulo(articulo)
+        case _:
+            print("Opción inválida")
+    time.sleep(1)
