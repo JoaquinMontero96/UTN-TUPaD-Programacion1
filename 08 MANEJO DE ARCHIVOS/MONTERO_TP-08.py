129527862954eb3e67fbd24b480114030aaf1dc3
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-10-30 00:05:29 -0300

    Ejercicio 3 TP 8

--- 08 MANEJO DE ARCHIVOS/MONTERO_TP-08.py
+++ 08 MANEJO DE ARCHIVOS/MONTERO_TP-08.py
@@ -2,3 +2,9 @@ with open("08 MANEJO DE ARCHIVOS/productos.txt", "r") as archivo:
     for linea in archivo:
         datos = linea.strip().split(",")
         print(f"Producto: {datos[0]} | Precio: {datos[1]} | Cantidad: {datos[2]}")
+
+with open("08 MANEJO DE ARCHIVOS/productos.txt", "a") as archivo:
+    producto = input("Ingrese un nuevo producto: ")
+    precio = float(input("Ingrese su precio: "))
+    stock = int(input("Ingrese la cantidad: "))
+    archivo.write(f"\n{producto},{precio},{stock}\n")
