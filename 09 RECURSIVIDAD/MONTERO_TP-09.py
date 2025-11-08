05040a380795ba73adfa70037327e62b4b937256
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-11-08 13:18:59 -0300

    Ejercicio 3 TP 9

--- 09 RECURSIVIDAD/MONTERO_TP-09.py
+++ 09 RECURSIVIDAD/MONTERO_TP-09.py
@@ -15,22 +15,44 @@
 
 # mostrar_factoriales()
 
-# Ejercicio 2
-def fibonacci(posicion):
-    if posicion == 0 or posicion == 1:
-        return posicion
-    else:
-        return fibonacci(posicion - 1) + fibonacci(posicion - 2)
+# # Ejercicio 2
+# def fibonacci(posicion):
+#     if posicion == 0 or posicion == 1:
+#         return posicion
+#     else:
+#         return fibonacci(posicion - 1) + fibonacci(posicion - 2)
 
-def mostrar_fibonacci():
+# def mostrar_fibonacci():
+#     try:
+#         num = int(input('Ingrese un número entero: '))
+#     except:
+#         print('Número inválido')
+#     if num < 1:
+#         print('El número debe ser mayor a 1')
+#     else:
+#         for i in range(1, num + 1):
+#             print(f'Fibonacci {i}: {fibonacci(i)}')
+
+# mostrar_fibonacci()
+
+# Ejercicio 3
+def potencia_rec(base, exponente):
+    if exponente == 0:
+        return 1
+    if exponente > 0:
+        return base * potencia_rec(base, exponente - 1)
+
+def probar_potencia():
     try:
-        num = int(input('Ingrese un número entero: '))
+        base = float(input('Ingrese la base: '))
+        exponente = int(input('Ingrese el exponente (entero): '))
     except:
-        print('Número inválido')
-    if num < 1:
-        print('El número debe ser mayor a 1')
-    else:
-        for i in range(1, num + 1):
-            print(f'Fibonacci {i}: {fibonacci(i)}')
+        print('Entrada inválida')
+        return
+    if exponente < 0:
+        print('El exponente debe ser un entero no negativo')
+        return
+    resultado = potencia_rec(base, exponente)
+    print(f'{base} elevado a {exponente} = {resultado}')
 
-mostrar_fibonacci()
+probar_potencia()
