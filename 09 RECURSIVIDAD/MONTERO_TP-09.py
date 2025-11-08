64b91986e0450d446d64cba6ceb857001b2fa0c0
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-11-08 12:56:21 -0300

    Ejercicio 2 TP 9

--- 09 RECURSIVIDAD/MONTERO_TP-09.py
+++ 09 RECURSIVIDAD/MONTERO_TP-09.py
@@ -1,16 +1,36 @@
-# Ejercicio 1
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+# # Ejercicio 1
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
 
-def mostrar_factoriales():
+# def mostrar_factoriales():
+#     try:
+#         num = int(input('Ingrese un número entero: '))
+#     except:
+#         print('Número inválido')
+#     for i in range(1, num+1):
+#         print(f'Factorial de {i}: {factorial(i)}')
+
+# mostrar_factoriales()
+
+# Ejercicio 2
+def fibonacci(posicion):
+    if posicion == 0 or posicion == 1:
+        return posicion
+    else:
+        return fibonacci(posicion - 1) + fibonacci(posicion - 2)
+
+def mostrar_fibonacci():
     try:
         num = int(input('Ingrese un número entero: '))
     except:
         print('Número inválido')
-    for i in range(1, num+1):
-        print(f'Factorial de {i}: {factorial(i)}')
+    if num < 1:
+        print('El número debe ser mayor a 1')
+    else:
+        for i in range(1, num + 1):
+            print(f'Fibonacci {i}: {fibonacci(i)}')
 
-mostrar_factoriales()
+mostrar_fibonacci()
