2d024214356463af3297faf6235e818cb2d5a47e
Joaquin Montero <joaquingmontero1996@gmail.com>
2025-11-15 11:14:13 -0300

    Ejercicio 6 TP 9

--- 09 RECURSIVIDAD/MONTERO_TP-09.py
+++ 09 RECURSIVIDAD/MONTERO_TP-09.py
@@ -79,20 +79,41 @@
 
 # probar_entero_a_binario()
 
-# Ejercicio 5
-def es_palindromo(palabra):
-    palabra = palabra.replace(' ', '').lower()
-    if len(palabra) <= 1:
-        return True
-    if palabra[0] != palabra[-1]:
-        return False
-    return es_palindromo(palabra[1:-1])
+# # Ejercicio 5
+# def es_palindromo(palabra):
+#     palabra = palabra.replace(' ', '').lower()
+#     if len(palabra) <= 1:
+#         return True
+#     if palabra[0] != palabra[-1]:
+#         return False
+#     return es_palindromo(palabra[1:-1])
 
-def probar_es_palindromo():
-    palabra = input('Ingrese una palabra o frase: ')
-    if es_palindromo(palabra):
-        print(f'"{palabra}" es un palíndromo.')
+# def probar_es_palindromo():
+#     palabra = input('Ingrese una palabra o frase: ')
+#     if es_palindromo(palabra):
+#         print(f'"{palabra}" es un palíndromo.')
+#     else:
+#         print(f'"{palabra}" no es un palíndromo.')
+
+# probar_es_palindromo()
+
+# # Ejercicio 6
+def suma_digitos(n):
+    if n == 0:
+        return 0
     else:
-        print(f'"{palabra}" no es un palíndromo.')
+        return n % 10 + suma_digitos(n // 10)
 
-probar_es_palindromo()
+def probar_suma_digitos():
+    try:
+        num = int(input('Ingrese un número entero no negativo: '))
+    except:
+        print('Número inválido')
+        return
+    if num < 0:
+        print('El número debe ser no negativo')
+        return
+    resultado = suma_digitos(num)
+    print(f'La suma de los dígitos de {num} es: {resultado}')
+
+probar_suma_digitos()
